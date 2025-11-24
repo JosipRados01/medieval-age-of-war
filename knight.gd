46ad45ab7da068a328ecd330e61b900add82bbd3
Josip <josiprados01@gmail.com>
2025-11-24 17:46:56 +0100

    Part of fighting logic

--- knight.gd
+++ knight.gd
@@ -12,7 +12,7 @@ var damage := 30
 var can_attack_again_timer
 var movement_speed := 100
 # "player" | "enemy"
-var team := "player"
+@export var team := "player"
 
 var is_combat_mode: bool = false
 var is_attacking: bool = false
@@ -32,6 +32,10 @@ func _ready() -> void:
 	#spawn on the correct position on the path
 	point_on_path.progress_ratio = progress_on_path_precentage
 	position = point_on_path.position
+	
+	# flip the scene if this is an enemy unit
+	if(team == "enemy"):
+		scale.x = -1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -48,29 +52,77 @@ func _process(delta: float) -> void:
 				start_attack_animation()
 			else:
 				#reposition according to enemy. Ideal position for knight is in front of the enemy and in range for attack
-				reposition()
+				reposition(delta)
 	else:
 		#continue following the path
 		move_on_path(delta)
 
+func get_enemies_detected():
+	var enemies = []
+	var overlaping = enemyDetector.get_overlapping_bodies()
+	for enemy in overlaping:
+		if (enemy.has_method("_get_team")):
+			var enemy_team = enemy._get_team()
+			if team != enemy_team:
+				enemies.append(enemy)
+	return enemies
 
 func check_combat_mode() -> bool:
+	# the unit should engage combat mode if the EnemyDetector area has enemies in the enemy team
+	var enemies = get_enemies_detected()
+	if(enemies.size() > 0):
+		return true
 	return false
 
 func should_unit_attack() -> bool:
+	# the unit should attack if the Enemy_in_range area has enemies in the enemy team
+	var overlaping = enemy_in_range.get_overlapping_bodies()
+	for enemy in overlaping:
+		if (enemy.has_method("get_team")):
+			var enemy_team = enemy._get_team()
+			if team != enemy_team:
+				return true
 	return false
-	
+
+func _get_team():
+	return team
+
 func start_attack_animation():
 	pass
 	
-func reposition():
-	pass
+func reposition(delta):
+	# check if an enemy is already in range using should_unit_attack
+	var enemy_in_range = should_unit_attack()
+	if(enemy_in_range):
+		return
+		
+	# get the position of the closest enemy detected
+	var overlaping_enemies = get_enemies_detected()
+	if overlaping_enemies.size() == 0: 
+		return
+	# for now just get the first one, but this should be the closest enemy
+	var chosen_enemy = overlaping_enemies[0]
+	
+	# if not find a point in front of the enemy to move towards
+	var targetPosition = chosen_enemy.position
+	if(team == "player"):
+		targetPosition.x -= 80 
+	else:
+		targetPosition.x += 80 
+		
+	# move to the point
+	position = position.move_toward(targetPosition, movement_speed * delta)
 	
 func move_on_path(delta):
-	#print()
 	#get and update the point
-	point_on_path.progress_ratio = progress_on_path_precentag
-	point_on_path.progress += movement_speed * delta
+	point_on_path.progress_ratio = progress_on_path_precentage
+	
+	#player's units move forward while enemy units move backwards
+	if(team == "player"):
+		point_on_path.progress += movement_speed * delta
+	else:
+		point_on_path.progress -= movement_speed * delta
+	
 	#update our knight for next frame
 	progress_on_path_precentage = point_on_path.progress_ratio
 
