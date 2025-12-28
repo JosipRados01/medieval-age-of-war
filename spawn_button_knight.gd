c706d9cba7b1ab4a79966b7d3e17a3c1a5049381
Josip <josiprados01@gmail.com>
2025-12-28 16:04:42 +0100

    Buttons for spawning units

---
+++ spawn_button_knight.gd
@@ -0,0 +1,21 @@
+extends TextureButton
+
+func _ready() -> void:
+	# Set button textures
+	texture_normal = load("res://assets/ui/buttons/spawn_knight_regular.png")
+	texture_pressed = load("res://assets/ui/buttons/spawn_knight_pressed.png")
+	stretch_mode = TextureButton.STRETCH_KEEP_ASPECT_CENTERED
+	ignore_texture_size = true
+
+func _on_pressed() -> void:
+	# Trigger the spawn_knight input action
+	var event_press = InputEventAction.new()
+	event_press.action = "spawn_knight"
+	event_press.pressed = true
+	Input.parse_input_event(event_press)
+	
+	# Send release event immediately
+	var event_release = InputEventAction.new()
+	event_release.action = "spawn_knight"
+	event_release.pressed = false
+	Input.parse_input_event(event_release)
